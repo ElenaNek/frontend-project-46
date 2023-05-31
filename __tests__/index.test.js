74fed488e1a22ad8809a1c9a9f006d681ae38940
Elena Nekrasova <leinyss@gmail.com>
2023-05-31 18:15:03 +0300

    h

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,10 +1,18 @@
 import process from 'node:process';
 import fs from 'node:fs';
 import * as path from 'path';
-import _ from 'lodash';
 import fileDiff from '../src/index.js';
+import { fileURLToPath } from 'url';
 
-const file = fileDiff();
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const getPathToFixture = (fileName) => path.join(__dirname, '..', '__fixtures__', fileName);
+
 
 test('first case', () => {
+    const data1 = fileDiff(getPathToFixture('file1.json'), getPathToFixture('file2.json'));
+    const result1 = fs.readFileSync(getPathToFixture('result.txt'), {encoding:'utf-8'});
+    console.log(data1);
+    console.log(result1);
+    expect(data1).toEqual(result1);
 });
