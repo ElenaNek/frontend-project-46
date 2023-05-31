3890163bcbbfc3e05e38320ee383de42c2c73b54
Elena Nekrasova <leinyss@gmail.com>
2023-05-31 21:02:23 +0300

    fixed

--- README.md
+++ README.md
@@ -4,6 +4,6 @@
 
 [![Maintainability](https://api.codeclimate.com/v1/badges/770517411395492883b9/maintainability)](https://codeclimate.com/github/ElenaNek/frontend-project-46/maintainability)
 
-[![GitHub Actions Demo](https://github.com/ElenaNek/frontend-project-46/actions/workflows/github-actions-demo.yml/badge.svg)](https://github.com/ElenaNek/frontend-project-46/actions/workflows/github-actions-demo.yml)
+[![ESLint](https://github.com/ElenaNek/frontend-project-46/actions/workflows/eslint.yml/badge.svg)](https://github.com/ElenaNek/frontend-project-46/actions/workflows/eslint.yml)
 
 [![Test Coverage](https://api.codeclimate.com/v1/badges/770517411395492883b9/test_coverage)](https://codeclimate.com/github/ElenaNek/frontend-project-46/test_coverage)
--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,18 +1,16 @@
-import process from 'node:process';
 import fs from 'node:fs';
 import * as path from 'path';
-import fileDiff from '../src/index.js';
 import { fileURLToPath } from 'url';
+import fileDiff from '../src/index.js';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const getPathToFixture = (fileName) => path.join(__dirname, '..', '__fixtures__', fileName);
 
-
 test('first case', () => {
-    const data1 = fileDiff(getPathToFixture('file1.json'), getPathToFixture('file2.json'));
-    const result1 = fs.readFileSync(getPathToFixture('result.txt'), {encoding:'utf-8'});
-    console.log(data1);
-    console.log(result1);
-    expect(data1).toEqual(result1);
+  const data1 = fileDiff(getPathToFixture('file1.json'), getPathToFixture('file2.json'));
+  const result1 = fs.readFileSync(getPathToFixture('result.txt'), { encoding: 'utf-8' });
+  console.log(data1);
+  console.log(result1);
+  expect(data1).toEqual(result1);
 });
